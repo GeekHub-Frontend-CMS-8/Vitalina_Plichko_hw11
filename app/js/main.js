ff10fcd1d766ab392eab21a86177a979b0ec6bf1
vitalina777 <vitalina.plichko52@gmail.com>
2019-01-19 11:57:10 +0200

    slider and ...

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,32 @@
-menu.onclick = function myFunction() {
-    var x = document.getElementById('myTopnav');
+// menu.onclick = function myFunction() {
+//     var x = document.getElementById('myTopnav');
+//
+//     if(x.className === "navigation"){
+//         x.className += " responsive";
+//     }else {
+//         x.className = "navigation";
+//     }
+// }
 
-    if(x.className === "navigation"){
-        x.className += " responsive";
-    }else {
-        x.className = "navigation";
-    }
-}
+$(document).ready(function () {
+    $(".homepage-top-slider").slick({});
+});
+
+// $(document).ready(function () {
+//     $('.mySlides').slick({});
+// });
+
+// $('.mySlides').slick();
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $('.mySlides').slick({
+//         arrows : false,
+//         dots : true,
+//     });
+// });
